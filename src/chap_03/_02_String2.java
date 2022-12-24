019d248ebc45fc5df81eba079f463fcdf4c9a7cc
jjenniyun <hjkjj1847@gmail.com>
2022-12-25 01:07:47 +0900

    프로젝트 다시 복구중(22.12.25)

---
+++ src/chap_03/_02_String2.java
@@ -0,0 +1,9 @@
+package chap_03;
+
+public class _02_String2 {
+    public static void main(String[] args) {
+        String s = "I like Java and Python and C.";
+        
+        // 문자열 반환
+    }
+}
